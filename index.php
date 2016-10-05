52ba6a66f75071e03d9905c9d775d0bcf605f11c
sla0193 <sla0193@vsb.cz>
2016-10-05 10:00:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="">	<!-- left to right -->
+  <head>  
+    <title>LOL</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />        
+</head>
+<body style="text-align: left; background-color: #112244; color: #FFFFFF;">
+<div style="background-color: black; width: 600px; left: 100px;">
+
+<?php
+//	header("Content-Type: text/html; charset=utf-8");
+	echo "Ale Lala mě pěkně nasrala. Utrhla Dipsimu anténku.";					/*print - to same co echo*/
+	for ($i = 0; $i < 10; $i++){
+		echo "<ul><li>Číslo " . $i . "</li></ul>";
+	}
+?>
+</div>
+
+</body>
+</html>
