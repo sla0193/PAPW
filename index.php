352c216907852af0200098b0e4afd36087732b73
sla0193 <sla0193@vsb.cz>
2016-10-12 10:02:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="">	<!-- left to right -->
+  <head>  
+    <title>LOL</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />        
+</head>
+<body style="text-align: left; background-color: #112244; color: #FFFFFF; font-family: Arial; font-size: 14px;">
+<div style="position: relative;
+  top: 0px;
+  width: 900px;
+  left: 50%;
+  margin-left: -450px;
+  margin-top: 0px;
+  background: transparent url('https://s-media-cache-ak0.pinimg.com/originals/7f/81/ce/7f81ce29a09f47eb1dcf030dbafa317f.jpg') no-repeat scroll center top;
+  padding: 20px;">
+
+<form>
+<p>Číslo: <input name="pocet" value="10" /></p>
+<p>Text: <input name="pismeno" value="Žjůůůva drak!"/></p>
+<p>Co chceš?</p>
+<p>
+	<select name="typSeznamu">
+		<option value="odr">odrážky</option>
+		<option value="cs" selected="selected">číselný</option>
+	</select>
+</p>	
+<p><input type="submit" /></p>
+</form>
+
+
+<?php
+	$cislo = $_REQUEST["pocet"];
+	$text = $_REQUEST["pismeno"];
+	echo "Zadane cislo: " . $cislo . "<br />Zadany text: " . $text . "<br />";	//cteni z formulare
+	echo "<br />Ale Lala mě pěkně nasrala. Utrhla Dipsimu anténku.";					/*print - to same co echo*/
+	
+	if($_REQUEST["typSeznamu"] == "cs"){
+		echo "<ol>";	
+		for ($i = 0; $i < $cislo; $i++){
+			echo "<li>" . $text . "</li>";
+		}
+		echo "</ol>";
+	}else{
+		echo "<ul>";
+		for ($i = 0; $i < $cislo; $i++){
+			echo "<li>" . $text . "</li>";
+		}
+		echo "</ul>";
+	}
+?>
+<p><img src="http://49.media.tumblr.com/7a92cb88e907df06e0569ab884d4067c/tumblr_nzek8grXXU1smshebo1_500.gif" /></p>
+</div>
+
+</body>
+</html>
