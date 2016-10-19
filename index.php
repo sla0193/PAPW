d4299f3b70b6cc8cc295eec562ae9e7e378b7ddd
sla0193 <sla0193@vsb.cz>
2016-10-19 10:31:12 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -19,25 +19,62 @@
   padding: 20px;">
 
 <form>
-<p>Číslo: <input name="pocet" value="10" /></p>
-<p>Text: <input name="pismeno" value="Žjůůůva drak!"/></p>
-<p>Co chceš?</p>
-<p>
-	<select name="typSeznamu">
-		<option value="odr">odrážky</option>
-		<option value="cs" selected="selected">číselný</option>
-	</select>
-</p>	
-<p><input type="submit" /></p>
+	<p>Číslo: <input name="pocet" value="10" /></p>
+	<p>Text: <input name="pismeno" value="Žjůůůva drak!"/></p>
+	<p>Si vyber:
+		<select name="typSeznamu">
+			<option value="odr">odrážky</option>
+			<option value="cs" selected="selected">číselný</option>
+		</select>
+	</p>	
+	
+	<p>
+		<textarea name="policko" rows="10" cols="50">Funguje to, i když je Ostrava napsána na konci věty a za ní je tečka. Tady je pokus. Město Ostrava.</textarea>
+	</p>
+	<p><input type="submit" /></p>
 </form>
 
 
 <?php
 	$cislo = $_REQUEST["pocet"];
 	$text = $_REQUEST["pismeno"];
-	echo "Zadane cislo: " . $cislo . "<br />Zadany text: " . $text . "<br />";	//cteni z formulare
-	echo "<br />Ale Lala mě pěkně nasrala. Utrhla Dipsimu anténku.";					/*print - to same co echo*/
+/*	echo "Zadane cislo: " . $cislo . "<br />Zadany text: " . $text . "<br />";	//cteni z formulare
+	echo "<br />Ale Lala mě pěkně nasrala. Utrhla Dipsimu anténku.";	*/				/*print - to same co echo*/
 	
+	$string = $_REQUEST["policko"];
+	echo "<p>".$string."</p>";
+	echo "<p>Počet znaků: ".strlen($string)."</p>";
+	
+	$pieces = explode(" ", $string);
+	
+		
+	echo "<p>";
+		for ($i = 0; $i < count($pieces); $i++){		//OR foreach ($pieces as $piece){
+			if(strcmp($pieces[$i],"Ostrava") == 0){
+				echo "<b>".$pieces[$i]." </b>";
+			}if(strcmp($pieces[$i],"Ostrava.") == 0){
+				$last = explode(".", $pieces[$i]);
+				if($last[0] == "Ostrava"){
+					echo "<b>".$last[0]."</b>. ";
+				}
+			}/*if(strcmp($pieces[$i],"Ostrava!") == 0){
+				$last = explode("!", $pieces[$i]);
+				if($last[0] == "Ostrava"){
+					echo "<b>".$last[0]."!</b> ";
+				}
+			}if(strcmp($pieces[$i],"Ostrava?") == 0){
+				$last = explode("?", $pieces[$i]);
+				if($last[0] == "Ostrava"){
+					echo "<b>".$last[0]."?</b> ";
+				}
+			}*/else{
+				echo $pieces[$i]." ";
+			}
+			
+		}
+	echo "<p>";
+	
+	/*
 	if($_REQUEST["typSeznamu"] == "cs"){
 		echo "<ol>";	
 		for ($i = 0; $i < $cislo; $i++){
@@ -50,7 +87,7 @@
 			echo "<li>" . $text . "</li>";
 		}
 		echo "</ul>";
-	}
+	}*/
 ?>
 <p><img src="http://49.media.tumblr.com/7a92cb88e907df06e0569ab884d4067c/tumblr_nzek8grXXU1smshebo1_500.gif" /></p>
 </div>
